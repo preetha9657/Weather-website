562faf109c3dbe9c3993934742407d681a8596e9
preetham <preetham9657@gmail.com>
2019-12-03 19:52:25 +0530

    add images and alter about page and css

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title:'Weather Report',
+        title:'Sexy Six',
         name:'Created by Preetham'
     })
 })
